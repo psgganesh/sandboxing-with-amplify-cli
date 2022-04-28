87de280538e482af760eb209d5f0419dc89fd92e
Shankar <psgganesh@gmail.com>
2022-04-28 23:06:17 +0800

    composable example

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
 <template>
     <h1 class="bg-green-100 text-green-600 text-center">Homepage</h1>
     <Counter />
+    <h1 class="text-xl text-indigo-500">x : {{ x }}</h1>
+    <h1 class="text-xl text-emerald-500">y: {{ y }}</h1>
 </template>
+
+<script setup>
+    const { x, y } = useMouse()
+</script>
