46cdc4e8ee3a6a25e0342e56c7ed30c2dda47d1c
Shankar <psgganesh@gmail.com>
2022-04-28 10:35:33 +0800

    project init

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  // Defaults options
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    config: {},
+    injectPosition: 0,
+    viewer: true,
+  }
+})
